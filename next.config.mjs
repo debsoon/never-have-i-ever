ba5e9c57a9f2595ac6103ed5beaa4f3b1bb07e8e
debsoon <debsoon@gmail.com>
2025-04-19 19:30:20 -0400

    feat: add Farcaster manifest and allow iframe embedding

--- next.config.mjs
+++ next.config.mjs
@@ -43,6 +43,19 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *"
+          }
+        ],
+      },
+      {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Origin', value: '*' },
